a2a3268e998626e00246a0eecd3606f9bb63f5f2
Alphonse256 <doctorfipaca241@gmail.com>
2023-09-26 16:14:12 +0300

    Fetched a joke using fetch, await and async

--- Dad_Jokes/app.js
+++ Dad_Jokes/app.js
@@ -1,13 +1,31 @@
 const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById('joke');
 
-function getJoke(){
-    console.log("Clicked!")
+const apiKey = 'AN1tp6GcdJUi/wGLIOXjXQ==lgbRZcrKbhbtTAPd';
+const options = {
+    method: 'GET',
+    headers: {
+        'X-Api-Key': apiKey,
+    },
+};
+
+const apiURL = 'https://api.api-ninjas.com/v1/dadjokes?limit=1';
+
+async function getJoke(){
+    jokeEl.innerText = 'Updating...';
+    btnEl.disabled = true;
+    btnEl.innerText = 'FETCHING...';
+
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
+    
+    jokeEl.innerHTML = data[0].joke;
+    btnEl.disabled = false;
+    btnEl.innerText = 'TELL ME A JOKE';
 }
 
 
 btnEl.addEventListener("click", getJoke);
-
-
 
 
 
